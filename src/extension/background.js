b25e5857d38c286474da7d0f79d351ab3b12c08e
Kevin Whitley <krwhitley@gmail.com>
2022-03-10 14:56:49 -0600

    chrome extension 101

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,12 +1,22 @@
+const sites = {}
+
+const getCSS = async path => {
+  const { hostname } = new URL(path)
+  const css = sites[hostname] || 'html * { background-color: pink; }'
+
+  return css
+          .replace(/;/g, ' !important;')
+          .replace(/\n([\.\w#])/g, 'html $1')
+}
+
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   const { status } = changeInfo
   const [{ url }] = await chrome.tabs.query({ active: true, currentWindow: true })
 
   if (status === 'complete') {
-    // chrome.scripting.insertCSS({
-    //   target: { tabId: tab.id },
-    //   css: await getCSS(url),
-    // })
-    alert(`tab switched to ${url}`)
+    chrome.scripting.insertCSS({
+      target: { tabId: tab.id },
+      css: await getCSS(url),
+    })
   }
 })
